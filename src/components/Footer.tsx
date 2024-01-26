0113409b3d1fbab55214d6376928c83b75f34305
RHB <rhb2020@gmail.com>
2024-01-26 15:29:04 +0330

    footer refactoring

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,19 @@
 function Footer() {
   return (
     <div className="Footer">
-      © کلیه حقوق برای بانک صادرات ایران محفوظ می باشد.
+      <span>© کلیه حقوق برای بانک صادرات ایران محفوظ می باشد. </span>
+      <span>
+        ایمیل:{" "}
+        <a href="mailto:rhb2020@gmail.com" target="_blank">
+          rhb2020@gmail.com
+        </a>{" "}
+      </span>
+      <span>
+        ایتا:{" "}
+        <a href="https://eitaa.com/rhatami_eitaa" target="_blank">
+          rhatami_eitaa@
+        </a>{" "}
+      </span>
     </div>
   );
 }
